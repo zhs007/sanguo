d7c2481c6f1567068c9747d4355770a80958f73e
zhs007 <sssxueren@gmail.com>
2015-05-16 13:46:47 +0800

    增加csv转换工具，基于nodejs的，跨平台，修改xlsx文件，通过csvtools来生成最终csv文件

---
+++ tools/csvtools/csvtools.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zhs007 on 15/5/16.
+ */
+
+var xlsx2csv = require('xlsx2csv');
+
+var xlsxroot = '../../res/static/';
+var csvroot = '../../sanguo/Resources/res/csv/';
+
+var files = ['actioninfo'];
+
+for (var i = 0; i < files.length; ++i) {
+    xlsx2csv.xlsx2csv(xlsxroot + files[i] + '.xlsx', csvroot + files[i] + '.csv');
+
+    console.log(xlsxroot + files[i] + '.xlsx => ' + csvroot + files[i] + '.csv');
+}
