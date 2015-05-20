ee7bf3abf706e24dfee84a3f0728e4ec92cbf1e1
zhs007 <sssxueren@gmail.com>
2015-05-20 10:09:01 +0800

    add army

---
+++ sanguo/Classes/GameScene/Army.cpp
@@ -0,0 +1,19 @@
+﻿#include "Army.h"
+
+USING_NS_CC;
+
+Army::Army()
+{
+}
+
+Army::~Army()
+{
+}
+
+void Army::init(int personid, GameScene& scene)
+{
+}
+
+void Army::setPosition(float xx, float yy)
+{
+}
